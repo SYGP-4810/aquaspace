3f1b9bb090c45b2e7a5ebd3609d7d20425865057
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-08-22 03:34:43 +0530

    need to test admin sample page front controller: puplic/indedx not working

--- Public/index.php
+++ Public/index.php
@@ -10,14 +10,14 @@
 /**
  * Autoloader
  */
- 
+
 spl_autoload_register(function ($class) {
     $root = dirname(__DIR__);   // get the parent directory
     $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
     if (is_readable($file)) {
         require_once $root . '/' . str_replace('\\', '/', $class) . '.php';
     }
-}); 
+});
 
 
 /**
@@ -29,23 +29,23 @@ set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
 
 
- 
+
 
 /**
  * get the action,controller,namespace throgh json 
  */
 //get json values
 $data = json_decode(file_get_contents('php://input'), true);
-if (isset($data['namespace'])){
+if (isset($data['namespace'])) {
     $params['namespace'] = $data['namespace'];
 }
-if (isset($data['data'])){
+if (isset($data['data'])) {
     $params['data'] = $data['data'];
 }
-if(isset($data['controller'])){
+if (isset($data['controller'])) {
     $params['controller'] = $data['controller'];
 }
-if(isset($data['action'])){
+if (isset($data['action'])) {
     $params['action'] = $data['action'];
 }
 
@@ -53,36 +53,32 @@ if(isset($data['action'])){
 
 
 
-
-
-
-
 /**
-     * Convert the string with hyphens to StudlyCaps,
-     * e.g. post-authors => PostAuthors
-     *
-     * @param string $string The string to convert
-     *
-     * @return string
-     */
+ * Convert the string with hyphens to StudlyCaps,
+ * e.g. post-authors => PostAuthors
+ *
+ * @param string $string The string to convert
+ *
+ * @return string
+ */
 function convertToStudlyCaps($str)
-    {
-        return str_replace(' ', '', ucwords(str_replace('-', ' ', $str)));
-    }
+{
+    return str_replace(' ', '', ucwords(str_replace('-', ' ', $str)));
+}
 
 
 /**
-     * Convert the string with hyphens to camelCase,
-     * e.g. add-new => addNew
-     *
-     * @param string $string The string to convert
-     *
-     * @return string
-     */
+ * Convert the string with hyphens to camelCase,
+ * e.g. add-new => addNew
+ *
+ * @param string $string The string to convert
+ *
+ * @return string
+ */
 function convertToCamelCase($string)
-    {
-        return lcfirst(convertToStudlyCaps($string));
-    }
+{
+    return lcfirst(convertToStudlyCaps($string));
+}
 /**
  * reequire controllers
  */
@@ -91,26 +87,25 @@ function convertToCamelCase($string)
  * Routing
  */
 
-    $namespace = 'App\Controllers\\'; //default namespace
-    if (array_key_exists('namespace', $params)) {
-        $namespace .= $params['namespace'] . '\\';
-    }
-    $controller = $params['controller'];
-    $controller = convertToStudlyCaps($controller);
-    $controller = $namespace . $controller;
+$namespace = 'App\Controllers\\'; //default namespace
+if (array_key_exists('namespace', $params)) {
+    $namespace .= $params['namespace'] . '\\';
+}
+$controller = $params['controller'];
+$controller = convertToStudlyCaps($controller);
+$controller = $namespace . $controller;
 
-    if (class_exists($controller)) {
-        $controller_object = new $controller($params);
+if (class_exists($controller)) {
+    $controller_object = new $controller($params);
 
-        $action = $params['action'];
-        $action = $convertToCamelCase($action);
+    $action = $params['action'];
+    $action = $convertToCamelCase($action);
 
-        if (is_callable([$controller_object, $action])) {
-            $controller_object->$action();
-
-        } else {
-            throw new \Exception("Method $action (in controller $controller) not found");
-        }
+    if (is_callable([$controller_object, $action])) {
+        $controller_object->$action();
     } else {
-        throw new \Exception("Controller class $controller not found");
+        throw new \Exception("Method $action (in controller $controller) not found");
     }
+} else {
+    throw new \Exception("Controller class $controller not found");
+}
