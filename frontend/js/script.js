999f27757515d5d5f821492c88d4d1b3c0ab308a
Inuri Gunasekara <57760444+inurim@users.noreply.github.com>
2021-09-25 23:43:07 +0530

    Fix navigation and checkboxes

---
+++ frontend/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(".select").click(function() {
+		$(".select").toggle('slow');
+		$("#form-content").toggle('slow');
+	});
+});
+
+// document.querySelector('.select').addEventListener('click', function()
+// 	{
+// 		document.getElementById("signup").style.display = "none"
+// 	}
+// );
--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,12 +1,18 @@
 $(".tab:not(:first)").hide();
+$(".form-delivery").hide();
 
 // Selecting user type 
-$(".submit").click(function(e) {
-
+$(".next").click(function(e) {
     $(".tab").hide();
     $(".tab").eq($(".tab").index()).show();
+    
+    // Update step
+    var x = document.getElementsByClassName("step");
+    x[0].className = x[0].className.replace(" active", " disabled");
+    x[1].className += " active";
 })
 
+// verify email
 $(".verify").click(function(e){
     $(".tab").hide();
 
@@ -17,6 +23,16 @@ $(".verify").click(function(e){
     } else if($("input[name='selection']:checked").val() == 'value-3') {
         $(".tab").eq(4).show();
     }
+
+    var x = document.getElementsByClassName("step");
+    x[1].className = x[1].className.replace(" active", " disabled");
+    x[2].className += " active";
+})
+
+// form submission
+$(".submit").click(function(e){
+    // insert what needs to happen
+    alert("Sign Up Complete");
 })
 
 // function validate()
