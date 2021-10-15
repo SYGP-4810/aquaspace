7a09e1c1e1deb9013ee2ac2956acfeb0bfb5af4a
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-10-15 19:30:10 +0530

    add few lines to update password in admin

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -16,22 +16,23 @@ $("#signIn").click(function(){
     var password = $("#password").val();
     var email = $("#email").val();
     var errFlag = 0;
+    var errors = [];
     //email should be acording to the structure
     if(!isEmail(email)){
-        alert("Please enter a valid email address");
+        errors.push("Please enter a valid email address");
         $("#email").focus();
         errFlag++;
     }
     //password should contains atleast one simple letter
     var lower = /(?=.*[a-z])/;
     if (!lower.test(password)){
-        alert("Please enter a valid password");
+        errors.push("Please enter a valid password");
         $("#password").focus();
         errFlag++;
     }
     //password should contain atleast 8 characters
     if(password.length < 8) {
-        alert("Password must be at least 8 characters long");
+        errors.push("Password must be at least 8 characters long");
         $("#password").focus();
         errFlag++;
     }
@@ -40,7 +41,7 @@ $("#signIn").click(function(){
     var upper = /(?=.*[A-Z])/;
   //password should contain atleast one capital letter
   if(!upper.test(password)){
-      alert("password should contain atleast one capital letter");
+      errors.push("password should contain atleast one capital letter");
       $("#password").focus();
       errFlag++;
   }
@@ -48,15 +49,14 @@ $("#signIn").click(function(){
     //password should contain atleast one number digit
     const regDig = /\d/;
     if(!regDig.test(password)){
-        alert("password should contain atleast one digit");
+        errors.push("password should contain atleast one digit");
         $("#password").focus();
         errFlag++;
     }
-
     //password should contain atleast one special character
     var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
     if(!format.test(password)){
-        alert("password should contain atleast one special character");
+        errors.push("password should contain atleast one special character")
         $("#password").focus();
         errFlag++;
     }   
@@ -75,9 +75,11 @@ $("#signIn").click(function(){
       var status = data.status;
       if(status == 1 ||status == 2 || status == 3 || status == 4 || status == 6 || status == 7){
         var redirect = data.redirect;
+        console.log(data);
         window.location.replace(redirect);
       }else if(status == 5){
-        alert("invalid credintial \n you have "+5-data.attemp+"left");
+        console.log(data);
+        alert(5-data.attempt+" attempts left");
         $("#email").focus();
         $("#password").focus();
       }else if(status == 7){
@@ -90,6 +92,8 @@ $("#signIn").click(function(){
     }
 });
     
+}else{
+  alert(JSON.stringify(errors));
 }
       
 });
