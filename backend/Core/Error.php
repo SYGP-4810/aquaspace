36fa22fd8edbc361c4ade2845f31920e221aad6d
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-10-24 12:36:42 +0530

    error login fixed

--- backend/Core/Error.php
+++ backend/Core/Error.php
@@ -56,10 +56,10 @@ class Error
             $log = dirname(__DIR__) . '/logs/' . date('Y-m-d') . '.txt';
             //ini_set('error_log', $log);
             $message = "Uncaught exception: '" . get_class($exception) . "'";
-            $message = "with error code : " . $code . "'";
+            $message .= "with error code : " . $code . "'";
             $message .= " with message '" . $exception->getMessage() . "'";
             $message .= "\nStack trace: " . $exception->getTraceAsString();
-            $message .= "\nThrown in '" . $exception->getFile() . "' on line " . $exception->getLine();
+            $message .= "\nThrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "\n\n";
             //write errors to a txt file
             $errorFile = fopen($log, "a");
             fwrite($errorFile, $message);
