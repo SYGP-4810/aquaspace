4c1a56024b4d5a30220ab1bfef0ccb61888c8724
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-10-25 13:36:50 +0530

    logo clicking problem solved

--- frontend/js/Admin/AdminAccountUpdate.js
+++ frontend/js/Admin/AdminAccountUpdate.js
@@ -5,7 +5,12 @@ $(document).ready(function(){
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data){
-            
+            console.log(data);
+            $("#fName").val(data.fName);
+            $("#lName").val(data.lName) ;
+            $("#address").val(data.address);
+            $("#city").val(data.city);
+            $("#telNo").val(data.tp);
         },
         error: function(errMsg) {
             //window.location.replace("/aquaspace/frontend/src/Error"+errMsg.status+".html");
