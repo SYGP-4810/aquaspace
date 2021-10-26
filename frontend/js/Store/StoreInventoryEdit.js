89337aadf33e42fc80f5831b9c02af1f44c6c716
wrlakshan <2019cs079@stu.ucsc.lk>
2021-10-26 21:33:27 +0530

    dila

--- frontend/js/Store/StoreInventoryEdit.js
+++ frontend/js/Store/StoreInventoryEdit.js
@@ -6,17 +6,18 @@ $(document).ready(function(){
     var url = new URL(window.location.href);
     var id = url.searchParams.get("id");
     var req = {"id":id}
-        $.ajax({
-            type: "POST",
-            url:setUrl("Admin/Admin/getAdminDetails"),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            data: JSON.stringify(req),
-            success: function(data){
+    console.log(req);
+        // $.ajax({
+        //     type: "POST",
+        //     url:setUrl("Admin/Admin/getAdminDetails"),
+        //     contentType: "application/json; charset=utf-8",
+        //     dataType: "json",
+        //     data: JSON.stringify(req),
+        //     success: function(data){
 
-            },
-            error: function(errMsg) {
-                // window.location.replace("../src/Error"+errMsg.status+".html");
-            }
-        })
+        //     },
+        //     error: function(errMsg) {
+        //         // window.location.replace("../src/Error"+errMsg.status+".html");
+        //     }
+        // })
 })
