2e8f42c11e4faae5d704374e3f46bcf5ccea190e
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-10-30 16:30:57 +0530

    question view reg user add

---
+++ frontend/js/Reg/questions.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url:setUrl("Reg/Reg/getExpertQuestions"),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+            
+            data.forEach((element)=>{
+                let reply = "";
+            if(element.reply){
+                reply = element.reply;
+            }
+                $(".questions").append(`<div class="question">
+                <h4>Question</h4>
+                <p>${element.question}</p>
+                <h4>Reply</h4>
+                <p>${reply}</p>
+            </div>`)
+            });
+            
+        },
+        error: function(errMsg) {
+            //window.location.replace("/aquaspace/frontend/src/Error/"+errMsg.status+".html");
+        }
+    });
+
+});
