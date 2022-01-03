27192781ec0ad03d9ca030bee8d1d16973d5030f
Yaiya Sam <kalsha.sam26@gmail.com>
2022-01-03 17:32:54 +0530

    change css for cart.html

--- frontend/js/Reg/cart.js
+++ frontend/js/Reg/cart.js
@@ -2,53 +2,50 @@ function setUrl(text) {
   return "/aquaspace/backend/public/index.php?" + text;
 }
 
-// $(document).ready(function () {
-  // jQuery methods go here...
+  // $("#cart").click(function () {
+  //   $(".shopping-cart").css("display", "block");
+  //   $("#cart").css("color", "rgb(61, 61, 61)");
+  //   $(".checkout").css("display", "none");
+  //   $("#checkout").css("color", "#aaaaaa");
+  //   $(".complete").css("display", "none");
+  //   $("#complete").css("color", "#aaaaaa");
+  // });
 
-  $("#cart").click(function () {
-    $(".shopping-cart").css("display", "block");
-    $("#cart").css("color", "rgb(61, 61, 61)");
-    $(".checkout").css("display", "none");
-    $("#checkout").css("color", "#aaaaaa");
-    $(".complete").css("display", "none");
-    $("#complete").css("color", "#aaaaaa");
-  });
+  // $("#checkout").click(function () {
+  //   $(".checkout").css("display", "block");
+  //   $("#checkout").css("color", "rgb(61, 61, 61)");
+  //   $(".shopping-cart").css("display", "none");
+  //   $("#cart").css("color", "#aaaaaa");
+  //   $(".complete").css("display", "none");
+  //   $("#complete").css("color", "#aaaaaa");
+  // });
 
-  $("#checkout").click(function () {
-    $(".checkout").css("display", "block");
-    $("#checkout").css("color", "rgb(61, 61, 61)");
-    $(".shopping-cart").css("display", "none");
-    $("#cart").css("color", "#aaaaaa");
-    $(".complete").css("display", "none");
-    $("#complete").css("color", "#aaaaaa");
-  });
-
-  $("#complete").click(function () {
-    $(".complete").css("display", "block");
-    $("#complete").css("color", "rgb(61, 61, 61)");
-    $(".shopping-cart").css("display", "none");
-    $("#cart").css("color", "#aaaaaa");
-    $(".checkout").css("display", "none");
-    $("#checkout").css("color", "#aaaaaa");
-  });
+  // $("#complete").click(function () {
+  //   $(".complete").css("display", "block");
+  //   $("#complete").css("color", "rgb(61, 61, 61)");
+  //   $(".shopping-cart").css("display", "none");
+  //   $("#cart").css("color", "#aaaaaa");
+  //   $(".checkout").css("display", "none");
+  //   $("#checkout").css("color", "#aaaaaa");
+  // });
 
   $("#proceed").click(function () {
-    $(".checkout").css("display", "block");
+    $(".cart-checkout").css("display", "block");
     $("#checkout").css("color", "rgb(61, 61, 61)");
     $(".shopping-cart").css("display", "none");
     $("#cart").css("color", "#aaaaaa");
-    $(".complete").css("display", "none");
-    $("#complete").css("color", "#aaaaaa");
+    // $(".complete").css("display", "none");
+    // $("#complete").css("color", "#aaaaaa");
   });
 
-  $("#order").click(function () {
-    $(".complete").css("display", "block");
-    $("#complete").css("color", "rgb(61, 61, 61)");
-    $(".cart").css("display", "none");
-    $("#cart").css("color", "#aaaaaa");
-    $(".checkout").css("display", "none");
-    $("#checkout").css("color", "#aaaaaa");
-  });
+  // $("#order").click(function () {
+  //   $(".complete").css("display", "block");
+  //   $("#complete").css("color", "rgb(61, 61, 61)");
+  //   $(".cart").css("display", "none");
+  //   $("#cart").css("color", "#aaaaaa");
+  //   $(".checkout").css("display", "none");
+  //   $("#checkout").css("color", "#aaaaaa");
+  // });
 
   $.ajax({
     type: "GET",
