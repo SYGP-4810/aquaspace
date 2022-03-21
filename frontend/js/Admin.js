871ddea669861db8c947cc96849d1124c0bef1dd
dilshansankalpa <dilshansankalpa98@gmail.com>
2022-03-21 21:18:09 +0530

    testing started

--- frontend/js/Admin.js
+++ frontend/js/Admin.js
@@ -74,7 +74,7 @@ $("#save123").click(function(){
             },
             error: function(errMsg) {
                 loadingFinish();
-                window.location.replace("/aquaspace/frontend/src/Error/"+errMsg.status+".html");
+                // window.location.replace("/aquaspace/frontend/src/Error/"+errMsg.status+".html");
             }
         });
 
--- frontend/js/expert/requests.js
+++ frontend/js/expert/requests.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
                     
                 </div>
             </div>
-                `)
+                `) 
             });
         },
         error: function(errMsg) {
