bd1fad8fce9c3a890f2be5f6e9d992f21f91de59
wrlakshan <2019cs079@stu.ucsc.lk>
2022-03-22 19:44:21 +0530

    expert blog

--- frontend/js/expert/requests.js
+++ frontend/js/expert/requests.js
@@ -9,12 +9,15 @@ $(document).ready(function() {
             console.log(data);
             data.forEach(element =>{
                 $("#verify").append(`
-                <div style="margin-left: 50px;"><a href="verify-post.html?id=${element.id}&name=${element.product_name}&img1=${element.img1}&img2=${element.img2}&img3=${element.img3}&img4=${element.img4}&authId=${element.auth_id}"><button id="claim">verify</button></a>
+                <div style="margin-left: 50px;">
                 <div style="display: flex; width: 400px;" class="question">
                     <img style="width: 150px; margin-right: 20px;" src="../../images/product/${element.img1}" alt="">
                     <div>
                     <h4>Fish Name :</h4>
                     <h4 style="font-weight: 400;">${element.product_name}</h4>
+                    <a href="verify-post.html?id=${element.id}&name=${element.product_name}&img1=${element.img1}&img2=${element.img2}&img3=${element.img3}&img4=${element.img4}&authId=${element.auth_id}">
+                        <button id="claim" style="top: 0px; left: 0px; background-color: rgb(57, 73, 145); padding: 4px 14px; border-radius: 4px; color: rgb(255, 255, 255);">verify</button>
+                    </a>
                     </div>
                     
                 </div>
